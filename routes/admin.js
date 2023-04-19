f2e6689b0a2abf090c1f1073147b1ebb9f271067
Abdul Arham <abdul.arham9123@gmail.com>
2023-04-19 21:21:39 +0500

    transferred mail functions to admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -11,6 +11,34 @@ const nodemailer = require("nodemailer");
 const fetchAdmin = require("../middleware/fetchAdmin");
 // const { json } = require("body-parser");
 
+// transporter function for connecting with the email to send messages
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "cinema.ticketing.system117@gmail.com",
+        pass: process.env.PASS
+    }
+})
+
+// function to generate the code
+function mailVerification(req, res, receiver) {
+    const verificationCode = Math.floor(100000 + Math.random() * 900000);
+    // generating the message
+    message = {
+        from: "cinema.ticketing.system117@gmail.com",
+        to: receiver,
+        subject: "Account Verification",
+        text: `Your Verification Code is ${verificationCode}`
+    }
+
+    // sending the mail to the user
+    transporter.sendMail(message, function (err, info) {
+        // Sending error message in case of any error
+        if (err) {return 0}
+    });
+    return verificationCode;
+}
+
 let verificationCode;
 
 // route for getting the admin
