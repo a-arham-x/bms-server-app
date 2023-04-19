1fc89aaa96a180a312a7db5ff711716708080135
Abdul Arham <abdul.arham9123@gmail.com>
2023-04-19 20:16:30 +0500

    commented some lines

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,14 @@
 // Making all the Necessary Imports
 const express = require("express");
 const router = express.Router();
-const mailVerification = require("../utils/emailVerfication.js");
+// const mailVerification = require("../utils/emailVerfication.js");
 const Customer = require("../models/Customer");
-const Order = require("../models/Orders")
-const { body, validationResult } = require("express-validator");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+// const Order = require("../models/Orders")
+// const { body, validationResult } = require("express-validator");
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
 const fetchAdmin = require("../middleware/fetchAdmin");
-const { json } = require("body-parser");
+// const { json } = require("body-parser");
 
 let verificationCode;
 
