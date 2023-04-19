dfc9d845e8f9223f67fb5c151e9b823f4bfc19ef
Abdul Arham <abdul.arham9123@gmail.com>
2023-04-19 19:37:33 +0500

    modified index.js

--- index.js
+++ index.js
@@ -1,39 +1,9 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
-
-// Making all the necessary imports
-const express = require("express");
-const app = express();
-const port = 5000;
-const connectToMongo = require('./utils/db');
-const bodyParser = require("body-parser");
-const fileUpload = require("express-fileupload");
-const cors = require("cors")
-
-// making the app able to use and upload temp files
-app.use(fileUpload({
-    useTempFiles: true
-}));
-
-// BodyParser for parsing the request bodies
-app.use(bodyParser.json());
-
-// Using Cors
-app.use(cors());
-
-// Using all the routes with the aid of the following middlewares
-// app.use(`/products`, require("./routes/products"));
-// app.use(`/customer`, require("./routes/customers"));
-// app.use(`/orders`, require("./routes/orders"));
-// app.use(`/admin`, require("./routes/admin"));
-
-// Calling the connectToMongo Function to connect to our database.
-connectToMongo();
+const express = require("express")
+const app = express()
+const port = 5000
 
 app.get("/", (req, res)=>{
-  res.send("Hello World");
+    res.send("Hello World")
 })
 
-// Starting the app
-app.listen(port);
+app.listen(port)
