1913b2b00924c19eeea6f962daa2ccf2ad2fc22c
Abdul Arham <abdul.arham9123@gmail.com>
2023-04-19 19:43:31 +0500

    remodified index.js

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-const express = require("express")
-const app = express()
-const port = 5000
+
+// Making all the necessary imports
+const express = require("express");
+const app = express();
+const port = 5000;
+const connectToMongo = require('./utils/db');
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const cors = require("cors")
+
+// making the app able to use and upload temp files
+app.use(fileUpload({
+    useTempFiles: true
+}));
+
+// BodyParser for parsing the request bodies
+app.use(bodyParser.json());
+
+// Using Cors
+app.use(cors());
+
+// Using all the routes with the aid of the following middlewares
+// app.use(`/products`, require("./routes/products"));
+// app.use(`/customer`, require("./routes/customers"));
+// app.use(`/orders`, require("./routes/orders"));
+// app.use(`/admin`, require("./routes/admin"));
+
+// Calling the connectToMongo Function to connect to our database.
+connectToMongo();
 
 app.get("/", (req, res)=>{
-    res.send("Hello World")
+  res.send("Hello World");
 })
 
-app.listen(port)
+// Starting the app
+app.listen(port);
